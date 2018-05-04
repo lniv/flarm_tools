369fa5222b90a9f3e5d040ca5c33e9ab5aec0d1f
Niv Levy <niv.levy@spacex.com>
2018-05-04 02:36:41 -0700

    change to 57600 - i'm sure i've been flying this way forever; not sure why this didn't get committed. adding receive code - borrowed from somewhere, don't think it's working, and i realized the hardware didn't connect the receive.

--- flarm2wifi/flarm2wifi.ino
+++ flarm2wifi/flarm2wifi.ino
@@ -1,11 +1,18 @@
 /* send flarm data over udp to e,g, tophat
- *  lots of hard coded stuff
+ * lots of hard coded stuff - ssid credentials.
+ * mean to run using an external router - i currently use a hootoo which integrates a sizeable battery.
+ * meant to run on an esp8266, obviously
  * as of jan 2nd 2016
+ * 
+ * compiled last May 4th 2018 , using arduino 1.6.8 board = node mcu 1.0 (ESP-12E)
+ * flow to flarm is good, pretty much confirming we're on port 2 of the flarm (since 1 was configured as 19200, while 2 was set to 57600 baud)
  */
 
 #include <ESP8266WiFi.h> 
 #include <WiFiUdp.h>
 //#include <ESP8266WebServer.h>
+
+#define ARRLEN(a) (sizeof(a)/sizeof((a)[0]))
 
 /* Set these to your desired credentials. */
 const char *ssid = "TripMateSith-0C78";
@@ -26,10 +33,11 @@ void send_packet(char *packetBuffer, int j) {
 }
 
 char packetBuffer[128];
+char wifi2serialBuffer [256];
 
 void setup() {
         delay(1000);
-        Serial.begin(19200); // match flarm setup
+        Serial.begin(57600); // match flarm setup ; seems we're connected on port 2
         /* You can remove the password parameter if you want the AP to be open. */
         
         WiFi.begin(ssid, password);
@@ -57,18 +65,29 @@ void setup() {
 
 byte j =0 ;
 void loop() {
-    int incomingByte;
+    int incomingByte, packetSize;
+	IPAddress remoteIp;
+	
     if (Serial.available() > 0) {
             incomingByte = Serial.read();
 //             Serial.println(incomingByte, DEC);
             packetBuffer[j++] = incomingByte;
             if ((j >0 && incomingByte =='\n') || j == 128) {
-                if (j < 128) {
+                if (j < ARRLEN(packetBuffer)) {
                     packetBuffer[j+1] == 0;
-                    Serial.println(packetBuffer);
+                    //Serial.println(packetBuffer);
                 }
                 send_packet(packetBuffer, j);
                 j = 0;
             }
     }
+	//handle incoming data
+    packetSize = udp.parsePacket();
+	if (packetSize > 0) {
+		remoteIp = udp.remoteIP(); // store the ip of the remote device
+		udp.read(wifi2serialBuffer, ARRLEN(wifi2serialBuffer));
+		// now send to UART:
+		Serial.write(wifi2serialBuffer, packetSize);
+	}
+  
 }
